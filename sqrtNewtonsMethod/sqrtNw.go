5506935d2b1b0fd97e0c59dfbe7aede8ca4317a5
Drew8Dino <lishchuk.andrii@zohomail.eu>
2021-04-20 19:25:20 +0300

    Realize sqrt function by Newton's method

---
+++ sqrtNewtonsMethod/sqrtNw.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const esp float64 = 0.000001
+
+func Sqrt(px *float64) float64 {
+	i := 0
+	z0, z1 := 1.0, 0.1
+	for math.Abs(z0*z0-*px) > esp {
+		z1 -= (z0*z0 - *px) / (2 * z0)
+		fmt.Println("Iteration number: ", i, "value z0: ", z0, "value z1: ", z1)
+		z0 = z1
+		i++
+	}
+	return z1
+
+}
+
+func main() {
+	x := 444.333222111
+	fmt.Println(Sqrt(&x))
+}
