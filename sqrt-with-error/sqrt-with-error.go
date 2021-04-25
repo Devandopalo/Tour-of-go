6ea4bd431b8871f8a8dc333baefadf7ae47cc560
Drew8Dino <lishchuk.andrii@zohomail.eu>
2021-04-25 19:39:01 +0300

    fix some errors

--- sqrt-with-error/sqrt-with-error.go
+++ sqrt-with-error/sqrt-with-error.go
@@ -13,9 +13,6 @@ func (e ErrNegativeSqrt) Error() string {
 	return fmt.Sprint("cannot Sqrt negative number: ", float64(e))
 }
 
-/*func Sqrt(x float64) (float64, error) {
-	return 0, nil
-}*/
 func Sqrt(x float64) (float64, error) {
 	if x < 0 {
 		return -1, ErrNegativeSqrt(x)
